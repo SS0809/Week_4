0cdd4e3b55da58983619f05744c37c941256ce9e
Saurabh Saraswat <saurabh45215@gmail.com>
2025-02-13 23:03:46 +0530

    [Saurabh] Add. 6. Filter Streams - Convert Uppercase to Lowercase

---
+++ Day3/src/main/org/filehandling/convertuppercasetolowercase/Main.java
@@ -0,0 +1,17 @@
+package org.filehandling.convertuppercasetolowercase;import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        String inputFilePath = "D:\\CapgTrainingAssignments\\input.txt";
+        String outputFilePath = "D:\\CapgTrainingAssignments\\output.txt";
+        FileConverter converter = new FileConverter();
+
+        try {
+            converter.convertFileToLowercase(inputFilePath, outputFilePath);
+            System.out.println("File has been converted to lowercase and saved as " + outputFilePath);
+        } catch (IOException e) {
+            System.out.println("Error occurred: " + e.getMessage());
+        }
+    }
+}
+
