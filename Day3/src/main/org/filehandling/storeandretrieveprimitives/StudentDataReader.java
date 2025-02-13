72e0d98bfbfcb0cccd662af10d09f370d5a40596
Saurabh Saraswat <saurabh45215@gmail.com>
2025-02-13 23:04:43 +0530

    [Saurabh] Add. 7. Data Streams - Store and Retrieve Primitive Data

---
+++ Day3/src/main/org/filehandling/storeandretrieveprimitives/StudentDataReader.java
@@ -0,0 +1,14 @@
+package org.filehandling.storeandretrieveprimitives;
+import java.io.*;
+
+public class StudentDataReader {
+    public static Student readStudentData(String fileName) throws IOException {
+        try (DataInputStream dataInputStream = new DataInputStream(new FileInputStream(fileName))) {
+            int rollNumber = dataInputStream.readInt();
+            String name = dataInputStream.readUTF();
+            double gpa = dataInputStream.readDouble();
+            return new Student(rollNumber, name, gpa);
+        }
+    }
+}
+
