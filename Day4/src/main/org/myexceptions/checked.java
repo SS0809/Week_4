1c0c81d2c0b7ca5be1d0b37d5e44636db90d71a9
Saurabh Saraswat <saurabh45215@gmail.com>
2025-02-14 13:11:35 +0530

    [Saurabh] Add. 1. Checked Exception (Compile-time Exception)

---
+++ Day4/src/main/org/myexceptions/checked.java
@@ -0,0 +1,24 @@
+package main.org.myexceptions;
+
+import java.io.*;
+
+//1. Checked Exception (Compile-time Exception)
+//💡 Problem Statement:
+//Create a Java program that reads a file named "data.txt". If the file does not exist, handle the IOException properly and display a user-friendly message.
+//Expected Behavior:
+//If the file exists, print its contents.
+//If the file does not exist, catch the IOException and print "File not found".
+public class checked {
+    public static void main(String[] args) {
+        try {
+            File fl = new File("input.txt");
+            BufferedReader f = new BufferedReader(new FileReader(fl));
+            String line ;
+            while ((line= f.readLine())!=null) {
+                System.out.print(line);
+            }
+        }catch (IOException e){
+            System.out.println("File not found");
+        }
+    }
+}
