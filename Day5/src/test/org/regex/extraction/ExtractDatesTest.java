e5f373da08a784fdbc9dbe5a3344abf7cca5fcc7
Saurabh Saraswat <saurabh45215@gmail.com>
2025-02-15 13:06:50 +0530

    [Saurabh] Add. {Extraction} 6. Extract Dates in dd/mm/yyyy Format && JUnit Tests

---
+++ Day5/src/test/org/regex/extraction/ExtractDatesTest.java
@@ -0,0 +1,19 @@
+package org.regex.extraction;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExtractDatesTest {
+    @Test
+    void test1() {
+        String input = "The events are scheduled for 12/05/2023, 15/08/2024, and 29/02/2020.";
+        ExtractDates vu = new ExtractDates();
+        vu.validate(input);
+        String[] expected = {"12/05/2023", "15/08/2024", "29/02/2020"};
+        System.out.println(vu.ans);
+        assertEquals(Arrays.asList(expected), vu.ans);
+    }
+}
