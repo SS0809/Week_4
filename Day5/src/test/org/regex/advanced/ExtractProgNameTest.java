87c8433dde1875d4a6a0697a2cbd9c59e2a6fe1f
Saurabh Saraswat <saurabh45215@gmail.com>
2025-02-15 15:32:42 +0530

    [Saurabh] Add. {Advance} 12. Extract Programming Language Names from a Text  && JUnit Tests

---
+++ Day5/src/test/org/regex/advanced/ExtractProgNameTest.java
@@ -0,0 +1,14 @@
+package org.regex.advanced;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExtractProgNameTest {
+    @Test
+    void test1(){
+        String expected = new String("Python, Go, Java");
+        ExtractProgName ep = new ExtractProgName();
+        assertEquals(expected,ep.extractLanguages("I love Java, Python, and JavaScript, but I haven't tried Go yet."));
+    }
+}
