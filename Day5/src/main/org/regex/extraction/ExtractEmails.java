b56c856bd40c296cfc58eeeb3fa0a249b4a4c18f
Saurabh Saraswat <saurabh45215@gmail.com>
2025-02-15 12:52:08 +0530

    [Saurabh] Add. {Extraction} 4. Extract All Email Addresses from a Text Number && JUnit Tests

--- Day5/src/main/org/regex/extraction/ExtractEmails.java
+++ Day5/src/main/org/regex/extraction/ExtractEmails.java
@@ -2,7 +2,9 @@ package org.regex.extraction;
 
 import org.regex.basic.HexColorCode;
 
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 //4️⃣ Extract All Email Addresses from a Text
@@ -12,11 +14,16 @@ import java.util.regex.Pattern;
 //support@example.com
 //info@company.org
 public class ExtractEmails {
-    boolean validate(String input){
+    ArrayList<String> ans = new ArrayList<>();
+    void validate(String input){
         //support@example.com
         //info@company.org
-        String regex = "^[0-9A-Za-z]+@[a-zA-Z]+.[a-z]{3}";
-        return Pattern.matches(regex, input);
+        String regex = "[0-9A-Za-z]+@[a-zA-Z]+.[a-z]{3}";
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(input);
+        while (m.find()){
+            this.ans.add(m.group());
+        }
     }
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
--- Day5/src/test/org/regex/extraction/ExtractEmailsTest.java
+++ Day5/src/test/org/regex/extraction/ExtractEmailsTest.java
@@ -3,25 +3,27 @@ package org.regex.extraction;
 import org.junit.jupiter.api.Test;
 import org.regex.extraction.ExtractEmails;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ExtractEmailsTest {
     @Test
-    void test1(){
-        String input = "info@company.org";
+    void test1() {
+        String input = "user at info@company.org from bhopal";
         ExtractEmails vu = new ExtractEmails();
-        assertEquals(true,vu.validate(input));
+        vu.validate(input);
+        String temp = new String("info@company.org");
+        ArrayList<String> expected = new ArrayList<String>();
+        expected.add(temp);
+        assertEquals(expected, vu.ans);
     }
     @Test
-    void test2(){
-        String input = "support@example.com";
+    void test2() {
+        String input = "user at info@@company.org from bhopal";
         ExtractEmails vu = new ExtractEmails();
-        assertEquals(true,vu.validate(input));
-    }
-    @Test
-    void test3(){
-        String input = "support@example.cm";
-        ExtractEmails vu = new ExtractEmails();
-        assertEquals(false,vu.validate(input));
+        vu.validate(input);
+        ArrayList<String> expected = new ArrayList<String>();
+        assertEquals(expected, vu.ans);
     }
 }
