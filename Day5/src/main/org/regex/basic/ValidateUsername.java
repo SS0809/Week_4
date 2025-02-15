3ffd276f0376fb5c2242230488a359f4bcb24b0e
Saurabh Saraswat <saurabh45215@gmail.com>
2025-02-15 12:08:17 +0530

    [Saurabh] Fix. {Basic} 1. Validate a Username

--- Day5/src/main/org/regex/basic/ValidateUsername.java
+++ Day5/src/main/org/regex/basic/ValidateUsername.java
@@ -18,13 +18,12 @@ public class ValidateUsername {
         //Can only contain letters (a-z, A-Z), numbers (0-9), and underscores (_)
         //Must start with a letter
         //Must be between 5 to 15 characters long
-        String regex = "^[a-zA-Z][a-zA-Z0-9_.+-]{4,14}";
+        String regex = "^[a-zA-Z][a-zA-Z0-9_]{4,14}";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(input);
         return pattern.matches(regex,input);
     }
     public static void main(String[] args) {
-        String input ;
         Scanner scn = new Scanner(System.in);
         ValidateUsername vu = new ValidateUsername();
         vu.validate(scn.nextLine());
