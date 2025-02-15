3a0021e5f114d08372547efaabb9e890e13f8c58
Saurabh Saraswat <saurabh45215@gmail.com>
2025-02-15 11:52:22 +0530

    [Saurabh] Add. {Basic} 1. Validate a Username && JUnit Tests

---
+++ Day5/src/main/org/regex/basic/RegexExample.java
@@ -0,0 +1,15 @@
+package org.regex.basic;
+
+import java.util.regex.*;
+public class RegexExample {
+    public static void main(String[] args) {
+        // Define the regex pattern
+        String regex = "\\d{3}";
+        Pattern pattern = Pattern.compile(regex);
+        String input = "The number is 123 and code is 456.";
+        Matcher matcher = pattern.matcher(input);
+        while (matcher.find()){
+            System.out.println(matcher.group());
+        }
+    }
+}
