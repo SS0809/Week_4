d77fbac081564e8d5481a45aa261973e65c830f8
Saurabh Saraswat <saurabh45215@gmail.com>
2025-02-15 15:07:06 +0530

    [Saurabh] Add. {ReplaceModify} 9. Validate an IP Address && JUnit Tests

---
+++ Day5/src/main/org/regex/advanced/ValidateIP.java
@@ -0,0 +1,22 @@
+package org.regex.advanced;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+//Validate an IP Address
+//A valid IPv4 address consists of four groups of numbers (0-255) separated by dots.
+public class ValidateIP {
+    boolean validate(String temp){
+        //        127.0.0.1
+        //A valid IPv4 address consists of four groups of numbers (0-255) separated by dots.
+        String regex = "^(25[0-5]|2[0-4][0-9]|1?[0-9][0-9]?)\\."+
+                "(25[0-5]|2[0-4][0-9]|1?[0-9][0-9]?)\\."+
+                "(25[0-5]|2[0-4][0-9]|1?[0-9][0-9]?)\\."+
+                "(25[0-5]|2[0-4][0-9]|1?[0-9][0-9]?)$";
+        return Pattern.matches(regex, temp);
+    }
+    public static void main(String[] args) {
+        ValidateIP ip = new ValidateIP();
+        System.out.println(ip.validate("127.0.0.1"));
+    }
+}
