941259e15890e8d6830a99302a5424cb46f25278
Saurabh Saraswat <saurabh45215@gmail.com>
2025-02-15 12:08:28 +0530

    [Saurabh] Add. {Basic} 2. Validate a License Plate Number && JUnit Tests

---
+++ Day5/src/main/org/regex/basic/LicensePlateNumber.java
@@ -0,0 +1,22 @@
+package org.regex.basic;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+//2️⃣ Validate a License Plate Number
+//License plate format: Starts with two uppercase letters, followed by four digits.
+//Example: "AB1234" is valid, but "A12345" is invalid.
+public class LicensePlateNumber {
+    boolean validate(String input){
+        //License plate format: Starts with two uppercase letters,
+        // followed by four digits.
+        String regex = "^[A-Z]{2}[0-9]{4}$";
+        return Pattern.matches(regex, input);
+    }
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        ValidateUsername vu = new ValidateUsername();
+        vu.validate(scn.nextLine());
+    }
+}
