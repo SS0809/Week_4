664cbea0d006c9584436600e1c7a1f68f5507485
Saurabh Saraswat <saurabh45215@gmail.com>
2025-02-15 12:52:18 +0530

    [Saurabh] Add. {Extraction} 5.  Extract All Capitalized Words from a Sentence && JUnit Tests

---
+++ Day5/src/main/org/regex/extraction/ExtractCapitalisedWord.java
@@ -0,0 +1,30 @@
+package org.regex.extraction;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+//5️⃣ Extract All Capitalized Words from a Sentence
+//🔹 Example Text:
+//        "The Eiffel Tower is in Paris and the Statue of Liberty is in New York."
+//        🔹 Expected Output:
+//Eiffel, Tower, Paris, Statue, Liberty, New, York
+public class ExtractCapitalisedWord {
+    ArrayList<String> ans = new ArrayList<>();
+    void validate(String input){
+        //All Capitalized Words from a Sentence
+        String regex = "[A-Z][a-z]+";
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(input);
+        while (m.find()){
+            this.ans.add(m.group());
+        }
+    }
+    public static void main(String[] args) {
+        String input = "The Eiffel Tower is in Paris and the Statue of Liberty is in New York.";
+        ExtractCapitalisedWord vu = new ExtractCapitalisedWord();
+        vu.validate(input);
+        System.out.println(vu.ans);
+    }
+}
---
+++ Day5/src/test/org/regex/extraction/ExtractCapitalisedWordTest.java
@@ -0,0 +1,20 @@
+package org.regex.extraction;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExtractCapitalisedWordTest {
+    @Test
+    void test1() {
+        String input = "The Eiffel Tower is in Paris and the Statue of Liberty is in New York.";
+        ExtractCapitalisedWord vu = new ExtractCapitalisedWord();
+        vu.validate(input);
+        String[] expected = {"The", "Eiffel", "Tower", "Paris", "Statue", "Liberty", "New", "York"};
+        System.out.println(vu.ans);
+        assertEquals(Arrays.asList(expected), vu.ans);
+    }
+}
