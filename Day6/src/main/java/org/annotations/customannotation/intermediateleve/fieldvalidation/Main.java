98c99f83de67f6c9819ec36edfb4ff0e36f6210d
Saurabh Saraswat <saurabh45215@gmail.com>
2025-02-17 15:03:17 +0530

    [Saurabh] Add.  Create a @MaxLength Annotation for Field Validation

---
+++ Day6/src/main/java/org/annotations/customannotation/intermediateleve/fieldvalidation/Main.java
@@ -0,0 +1,18 @@
+package org.annotations.customannotation.intermediateleve.fieldvalidation;
+public class Main {
+    public static void main(String[] args) {
+        try {
+            User user = new User("LongUsername"); // Should throw exception
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            User user = new User("Short"); // Should be valid
+            System.out.println("Username: " + user.getUsername());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
+
