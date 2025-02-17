afec4be6dddc6a73e22b297bf6dc060132edf3f0
Saurabh Saraswat <saurabh45215@gmail.com>
2025-02-17 15:02:38 +0530

    [Saurabh] Add. Create a @Todo Annotation for Pending Tasks

---
+++ Day6/src/main/java/org/annotations/customannotation/beginnerlevel/pendingtasklist/Main.java
@@ -0,0 +1,6 @@
+package org.annotations.customannotation.beginnerlevel.pendingtasklist;
+public class Main {
+    public static void main(String[] args) {
+        TodoRetriever.retrievePendingTasks(TaskService.class);
+    }
+}
---
+++ Day6/src/main/java/org/annotations/customannotation/beginnerlevel/pendingtasklist/TaskService.java
@@ -0,0 +1,13 @@
+package org.annotations.customannotation.beginnerlevel.pendingtasklist;
+public class TaskService {
+
+    @Todo(task = "Implement login feature", assignedTo = "John")
+    public void login() {}
+
+    @Todo(task = "Create user profile page", assignedTo = "Alice", priority = "HIGH")
+    public void createProfile() {}
+
+    @Todo(task = "Implement logout feature", assignedTo = "John", priority = "LOW")
+    public void logout() {}
+}
+
