8b54398ccbc3b7e6647537dd096e891064aaa425
Saurabh Saraswat <saurabh45215@gmail.com>
2025-02-17 15:04:15 +0530

    [Saurabh] Add. Implement a Custom Caching System with @CacheResult

---
+++ Day6/src/main/java/org/annotations/customannotation/advancedlevel/customcachingsystem/CacheResult.java
@@ -0,0 +1,7 @@
+package org.annotations.customannotation.advancedlevel.customcachingsystem;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface CacheResult {
+}
